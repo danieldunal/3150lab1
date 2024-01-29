7dca90de6987cff334f4681933254833d49a1f86
danieldunal <144380099+danieldunal@users.noreply.github.com>
2024-01-29 09:45:25 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 Compilation instructions:
 
+Step 1: Go to the directory you wish to download and install the files
+
+Step 2: run the following command: git clone https://github.com/danieldunal/3150lab1.git
+
+Step 3: You can use the command $g++ main_prefix_sum.cpp to run main function with basic inputs.
+
+You can also run $g++ unit_tests_prefix_sum.cpp for basic unit tests.
--- main_prefix_sum.cpp
+++ main_prefix_sum.cpp
@@ -4,13 +4,13 @@
 
 int main() {
 
-    int ary[] = {1, -1, -1, 1, 1, -1};
+    int ary[] = {1, -1, 1, -1, 1, -1};
 
     int sum = prefix_sum(ary, sizeof(ary)/sizeof(ary[0]));
     bool pos = non_neg_prefix_sum(ary, sizeof(ary)/sizeof(ary[0]));
     bool neg = non_pos_prefix_sum(ary, sizeof(ary)/sizeof(ary[0]));
     
-    std::cout << sum << std::endl;
+    std::cout << sum << std::endl << pos << std::endl << neg << std::endl;
 
     return 0;
 }
